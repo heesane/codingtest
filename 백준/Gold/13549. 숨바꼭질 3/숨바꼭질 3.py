f36332c02e0c67204253361d1b9bc02e6d04be01
희상 <tees3359@gmail.com>
2024-02-20 17:17:00 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 164 ms, Memory: 114860 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,27 +1,24 @@
-# 백준 13549 숨바꼭질 3
-
 from collections import deque
 
-N,K = map(int, input().split())
+def bfs(start,end):
+    q = deque([start])
+    visited = [0] * MAX
+    while q:
+        x = q.popleft()
+        if x == end:
+            return visited[x]
+
+        for nx in (x-1,x+1,2*x):
+            if 0 <= nx < MAX and not visited[nx]:
+                if nx == 2*x and x!=0:
+                    visited[nx] = visited[x]
+                    q.appendleft(nx)
+                else:
+                    visited[nx] = visited[x] + 1
+                    q.append(nx)
 
 MAX = 100001
 
-visited = [0] * MAX
+N,K = map(int,input().split())
 
-def bfs():
-    queue = deque([N])
-    while queue:
-        x = queue.popleft()
-        if x == K:
-            print(visited[x])
-            return
-        for nx in (x-1, x+1, x*2):
-            if 0 <= nx < MAX and not visited[nx]:
-                if nx == x*2 and x != 0:
-                    visited[nx] = visited[x]
-                    queue.appendleft(nx)
-                else:
-                    visited[nx] = visited[x] + 1
-                    queue.append(nx)
-                    
-bfs()
+print(bfs(N,K))
