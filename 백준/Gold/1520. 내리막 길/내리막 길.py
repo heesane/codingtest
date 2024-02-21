4b2308b75221282e09efbfbe0f7796c53f43cd63
희상 <tees3359@gmail.com>
2024-02-22 00:13:09 +0900

    [Gold III] Title: 내리막 길, Time: 272 ms, Memory: 143096 KB -BaekjoonHub

---
+++ 백준/Gold/1520. 내리막 길/내리막 길.py
@@ -0,0 +1,29 @@
+# 백준 1520 내리막 길
+from collections import deque
+
+M, N = map(int,input().split())
+dp = [[-1] * N for _ in range(M)]
+graph = [list(map(int,input().split())) for _ in range(M)]
+
+dx,dy = [1,-1,0,0],[0,0,1,-1]
+
+def dfs(x,y):
+    
+    if x == M-1 and y == N-1:
+        return 1
+    
+    if dp[x][y] != -1:
+        return dp[x][y]
+    
+    dp[x][y] = 0
+    
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        
+        if 0<= nx < M and 0 <= ny < N:
+            if graph[nx][ny] < graph[x][y]:
+                dp[x][y] += dfs(nx,ny)
+    return dp[x][y]
+
+print(dfs(0,0))
