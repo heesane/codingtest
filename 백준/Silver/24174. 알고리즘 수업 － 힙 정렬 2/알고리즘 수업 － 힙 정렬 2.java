716db3db639d9c1937f0d476ebcce65c19cbd5df
희상 <tees3359@gmail.com>
2024-04-21 00:11:51 +0900

    [Silver V] Title: 알고리즘 수업 - 힙 정렬 2, Time: 880 ms, Memory: 93332 KB -BaekjoonHub

--- 백준/Silver/24174. 알고리즘 수업 － 힙 정렬 2/알고리즘 수업 － 힙 정렬 2.java
+++ 백준/Silver/24174. 알고리즘 수업 － 힙 정렬 2/알고리즘 수업 － 힙 정렬 2.java
@@ -1,78 +1,117 @@
 // 백준 24174번 힙 정렬
 
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.Arrays;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+
+// max heap sort = 오름차순
+// min heap sort = 내림차순
 
 class Main{
     public static int cnt = 0, target = 0;
-    public static boolean isOut = false;
+    public static boolean flag = false;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st =new StringTokenizer(br.readLine());
 
-        int[] input  = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int n = input[0]; // 배열 A의 크기
-        target = input[1]; // 교환 횟수
+        int N = Integer.parseInt(st.nextToken());
+        target = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[n+1];
-        arr[0] = 0;
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 1; i < n+1; i++) arr[i] = Integer.parseInt(st.nextToken());
-
-        heap_sort(arr);
-        if(!isOut){
-            System.out.print(-1);
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N ; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        heapSort(arr);
+        if(!flag){
+            System.out.println(-1);
         }
     }
 
-    public static void swap(int[] arr,int i, int j){
+    // swap
+    // 해당 문제에서는 cnt에 맞춰서 StringBuilder로 출력
+    public static void swap(int[] arr ,int i, int j){
         cnt++;
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
 
-        if(cnt == target){
-            StringBuilder sb= new StringBuilder();
-            for (int k = 1; k < arr.length; k++) {
+        if(target == cnt){
+            StringBuilder sb = new StringBuilder();
+            for (int k = 0; k < arr.length; k++) {
                 sb.append(arr[k]).append(" ");
             }
-            System.out.print(sb);
-            isOut = true;
+            System.out.println(sb);
+            flag= true;
         }
     }
 
-    public static void heap_sort(int[] arr){
-        build_min_heap(arr,arr.length - 1);
-        for (int i = arr.length - 1; i > 1 ; i--) {
-            swap(arr,1,i);
-            heapify(arr,1,i-1);
-        }
-    }
-    public static void build_min_heap(int[] arr, int n){
-        for (int i = n/2; i >= 1 && !isOut; i--) {
-            heapify(arr,i,n);
-        }
-    }
-    public static void heapify(int[] arr, int k, int n){
-        int left = 2* k;
-        int right = 2*k+1;
-        int smaller = -1;;
+    // min heap 만드는 과정
+    public static void min_heapify(int[] arr, int parentIdx, int lastIdx){
+        int left= parentIdx * 2 + 1;
+        int right = parentIdx * 2 + 2;
+        int largestIdx = parentIdx;
 
-        if(right <= n){
-            smaller = arr[left] < arr[right] ? left : right;
+        if(left <= lastIdx && arr[left] < arr[largestIdx]){
+            largestIdx = left;
         }
-        else if (left <= n){
-            smaller = left;
-        }
-        else{
-            return;
+        if(right <= lastIdx && arr[right] < arr[largestIdx]){
+            largestIdx= right;
         }
 
-        if(arr[smaller] < arr[k]){
-            swap(arr,k,smaller);
-            heapify(arr,smaller,n);
+        if(parentIdx != largestIdx){
+            swap(arr,parentIdx,largestIdx);
+            min_heapify(arr,largestIdx,lastIdx);
+        }
+
+    }
+    // max heap 만드는 과정
+    public static void max_heapify(int[] arr, int parentIdx, int lastIdx){
+        int left= parentIdx * 2 + 1;
+        int right = parentIdx * 2 + 2;
+        int largestIdx = parentIdx;
+
+        if(left <= lastIdx && arr[left] > arr[largestIdx]){
+            largestIdx = left;
+        }
+        if(right <= lastIdx && arr[right] > arr[largestIdx]){
+            largestIdx = right;
+        }
+
+        if(parentIdx != largestIdx){
+            swap(arr,parentIdx,largestIdx);
+            max_heapify(arr,largestIdx,lastIdx);
+        }
+    }
+
+    // 부모노드 찾기
+    public static int getParent(int idx){
+        return (idx - 1) / 2;
+    }
+    public static void heapSort(int[] arr){
+        int size = arr.length;
+
+        if(size < 2) return;
+
+        // 부모노드 찾기
+        int parentIdx = getParent(size - 1);
+
+        // heap 만드는 과정
+        for (int i = parentIdx; i >= 0 ; i--) {
+            // max_heapify(arr,i,size - 1);
+            // 부모노드부터, 0번 인덱스까지
+            // 자식 노드의 -1 level부터 0 level까지 탐색하면서, 최대 및 최소 힙 생성
+            min_heapify(arr,i,size-1);
+
+        }
+
+        // 정렬하는 과정
+        for (int i = size - 1; i > 0  ; i--) {
+            // 맨 앞 노드와 뒤 노드 위치 전환
+            swap(arr,0,i);
+            // max_heapify(arr,0,i-1);
+            min_heapify(arr,0,i-1);
         }
     }
 
