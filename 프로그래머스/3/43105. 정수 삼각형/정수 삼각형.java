3422e8f8c9d51dd91f6f9db8970de76093318161
희상 <tees3359@gmail.com>
2024-06-16 18:30:32 +0900

    [level 3] Title: 정수 삼각형, Time: 8.31 ms, Memory: 57.9 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,20 +1,18 @@
+import java.util.*;
 class Solution {
     public int solution(int[][] triangle) {
         if(triangle.length == 1 ) return triangle[0][0];
-        // 어느 방향으로 가든 갈 수 있는 idx는 다음 레벨의 현재 idx, idx + 1
-        int answer = 0;
-        int[][] dp = new int[triangle.length+1][triangle.length];
-        
-        for(int i = 1; i < triangle.length+1; i++){ // 층 증가
-            for(int j = 0; j < triangle[i-1].length; j++){ // 해당 층에 있는 모든 노드 검사
-                for(int k = j-1; k < j+1; k++){ // 다음 층 검사
-                    if(k < 0) continue;
-                    dp[i][j] = Math.max(dp[i][j], dp[i-1][k] + triangle[i-1][j]);
-                    answer = Math.max(dp[i][j], answer);
-                }
+        for(int i = 1; i < triangle.length;i++){
+            triangle[i][0] += triangle[i-1][0];
+            triangle[i][i] += triangle[i-1][i-1];
+            
+            for(int j = 1; j < i; j++){
+                triangle[i][j] = Math.max(triangle[i-1][j-1] + triangle[i][j],triangle[i-1][j] + triangle[i][j]);
             }
         }
+        int[] res = triangle[triangle.length - 1];
         
-        return answer;
+        
+        return Arrays.stream(res).max().getAsInt();
     }
 }
